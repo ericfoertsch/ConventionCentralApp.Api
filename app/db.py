4a22a41a6fb6b5a3b555ffe3601168e0b854e7b8
Eric Foertsch <foertsce@gmail.com>
2025-05-04 19:40:47 -0400

    Update the db file to correctly query data.

--- app/db.py
+++ app/db.py
@@ -5,14 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "myuser")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "mypassword")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "mydatabase")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
 DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 )
 
 engine = create_engine(DATABASE_URL)
