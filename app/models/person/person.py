732157108f75c7e5513f41f4d5fde5486fb6539e
Eric Foertsch <foertsce@gmail.com>
2025-06-18 01:33:23 -0400

    Expand the mapped files.

--- app/models/person/person.py
+++ app/models/person/person.py
@@ -1,7 +1,12 @@
+from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 class Person(BaseModel):
     id_person: int
+    email: Optional[str]
+    description: Optional[str]
+    utc_begin: Optional[datetime]
 
     class Config:
         from_attributes = True
--- app/models/series/convention_series.py
+++ app/models/series/convention_series.py
@@ -1,7 +1,18 @@
+from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 class ConventionSeries(BaseModel):
     id_convention_series: int
+    description: Optional[str]
+    email: Optional[str]
+    id_image: Optional[int]
+    id_person: Optional[int]
+    id_video: Optional[int]
+    link: Optional[str]
+    name: Optional[str]
+    utc_begin: Optional[datetime]
+    utc_timestamp: Optional[datetime]
 
     class Config:
         from_attributes = True
